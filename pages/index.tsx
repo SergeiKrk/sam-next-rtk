56761d09c6d9fdd744ca966de1e44732084d5aef
Sergey Kryukov <krjukov_s@mail.ru>
2022-10-03 22:20:47 +0400

    added Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import Navbar from "../components/navbar/Navbar";
 import Layout from "../components/Layout/Layout";
 
 const Home: NextPage = () => {
   return (
     <>
+    <Navbar />
     <Layout />
     </>
   );
