53841c4c9906611e250bedb4d226c7ec86f3246b
ksv <krjukov_s@mail.ru>
2022-10-14 17:27:20 +0400

    refactoring shareheader

--- components/shareButtons/ShareButtons.js
+++ components/shareButtons/ShareButtons.js
@@ -22,42 +22,42 @@ const ShareButtons = ({ urlSeo, titlePage, headImg, descriptionPage }) => {
         <Link
           href={`http://vk.com/share.php?url=https://samogoncalc.ru/${urlSeo}&title=${titlePage}&image=https://samogoncalc.ru${headImg}&noparse=true`}
         >
-          <a className={s.btnBlock}>
+          <a className={`hover:bg-cyan-800 ${s.btnBlock}`}>
             <FaVk className="text-white" />
           </a>
         </Link>
         <Link
           href={`https://t.me/share/url?url=https://samogoncalc.ru/${urlSeo}&text=${titlePage}`}
         >
-          <a className={s.btnBlock}>
+          <a className={`hover:bg-cyan-500 ${s.btnBlock}`}>
             <FaTelegramPlane className="text-white" />
           </a>
         </Link>
         <Link
           href={`viber://forward?text=https://samogoncalc.ru/${urlSeo} | ${titlePage}`}
         >
-          <a className={s.btnBlock}>
+          <a className={` hover:bg-[#25D366] ${s.btnBlock}`}>
             <FaWhatsapp className="text-white" />
           </a>
         </Link>
         <Link
           href={`viber://forward?text=${titlePage} | https://samogoncalc.ru/${urlSeo}`}
         >
-          <a className={s.btnBlock}>
+          <a className={` hover:bg-purple-800 ${s.btnBlock}`}>
             <FaViber className="text-white" />
           </a>
         </Link>
         <Link
           href={`http://www.fb.com/sharer.php?s=100&p[title]=${titlePage}&p[summary]=${descriptionPage}&p[url]=https://samogoncalc.ru/${urlSeo}&p[images][0]=https://samogoncalc.ru${headImg}`}
         >
-          <a className={s.btnBlock}>
+          <a className={` hover:bg-indigo-900 ${s.btnBlock}`}>
             <FaFacebookF className="text-white" />
           </a>
         </Link>
         <Link
           href={`https://twitter.com/intent/tweet?original_referer=https://samogoncalc.ru/${urlSeo}&text=${titlePage}&url=https://samogoncalc.ru`}
         >
-          <a className={s.btnBlock}>
+          <a className={` hover:bg-blue-500 ${s.btnBlock}`}>
             <FaTwitter className="text-white" />
           </a>
         </Link>
