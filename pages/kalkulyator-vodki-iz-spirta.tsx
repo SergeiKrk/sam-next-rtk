2eec2f994cadfd287ffa038d0da9d317007ab8b7
ksv <krjukov_s@mail.ru>
2022-10-28 16:55:28 +0400

    added kalkulyator-vodki-iz-spirta vodkapage

---
+++ pages/kalkulyator-vodki-iz-spirta.tsx
@@ -0,0 +1,22 @@
+import type { NextPage } from "next";
+import Layout from "../components/layout/Layout";
+import VodkaPage from "../components/pages/Vodka/VodkaPage";
+
+const Vodka: NextPage = () => {
+  return (
+    <>
+      <Layout
+        headImg="/img/kalkulyator-vodki-iz-spirta.png"
+        titlePage="Калькулятор водки из спирта"
+        descriptionPage="Поможет подобрать пропорции и разбавить спирт или самогон водой до 40 градусов"
+        urlSeo="kalkulyator-vodki-iz-spirta/"
+        title="Как разбавить самогон водой до 40 градусов: онлайн калькулятор"
+        description="Онлайн калькулятор водки из спирта поможет подобрать пропорции и разбавить спирт или самогон водой до 40 градусов"
+      >
+        <VodkaPage />
+      </Layout>
+    </>
+  );
+};
+
+export default Vodka;
